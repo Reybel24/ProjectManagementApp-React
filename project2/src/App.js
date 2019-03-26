1e813674368d17d3bf85d69dbbbfa97d8012c977
Anastasia Mishin <am988@njit.edu>
2019-03-26 16:10:37 -0400

    App Component API Request

--- project2/src/App.js
+++ project2/src/App.js
@@ -1,29 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TaskList from './components/tasklist';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1> Thanks </h1>
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+
+    constructor(props){
+        super(props);
+        this.state = {
+            items: [],
+            isLoaded: false,
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://my-json-server.typicode.com/amishin/project2_db/posts')
+            .then(result => result.json())
+            .then(json => {
+                this.setState({
+                    isLoaded: true,
+                    items: json, //get data from api
+                })
+            });
+    }
+    taskStatusChanged = (id, column) => {
+        const itemIndex = this.state.items.findIndex(_=>_.id === id);
+        if(itemIndex !== -1){
+            const item = {...this.state.items[itemIndex], column};
+            const newItems = [...this.state.items];
+            newItems[itemIndex] = item;
+            this.setState({items: newItems });
+        }
+        console.log("taskStatusChanged", id, column);
+    }
+
+
+
+
+
+    render() {
+
+        var {isLoaded, items } = this.state; // access these items
+
+        if (!isLoaded) {
+            return <div> Loading...</div>
+        }
+
+        else {
+             return (
+                 <TaskList items={this.state.items} onStatusChanged={(id, status)=>this.taskStatusChanged(id, status)}/>
     );
-  }
+  } }
 }
 
 export default App;
