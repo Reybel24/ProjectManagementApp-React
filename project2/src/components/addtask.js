d3ddfa7041eaa6b1ee9634a9286937f4f2b22ff3
Anastasia Mishin <am988@njit.edu>
2019-04-09 13:05:43 -0400

    Added two other components

---
+++ project2/src/components/addtask.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+class AddTask extends Component {
+    state = {}
+}
+
+export default AddTask;
--- project2/src/components/tasklist.js
+++ project2/src/components/tasklist.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+
 class TaskList extends Component {
     state = {
         sort: {
